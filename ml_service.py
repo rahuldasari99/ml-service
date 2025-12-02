9f2f2b09dc4654440f72967d6da5775046ca0576
rahuldasari99 <rahuldasari592@gmail.com>
2025-12-02 19:57:55 +0530

    update

--- ml_service.py
+++ ml_service.py
@@ -389,5 +389,8 @@ def predict_price(req: PriceRequest):
 # Run server
 # ----------------------------------------------
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    import uvicorn, os
+
+    port = int(os.environ.get("PORT", 8000))   # Render gives PORT dynamically
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
